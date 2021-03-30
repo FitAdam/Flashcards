c848d01de7808f4d5edf4717889cca197f9c8dac
FitAdam <adam.george617@gmail.com>
2021-03-30 16:25:38 +0100

    members card

--- FlashCards/ClientApp/src/components/About.tsx
+++ FlashCards/ClientApp/src/components/About.tsx
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
+import ReactCardFlip from 'react-card-flip';
 import './About.css'
-import { Link } from 'react-router-dom';
 import Wood from '../resources/wood.jpg';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Button,
+} from 'reactstrap';
 
-const About = () => (
-  <div>
-    <div className="AboutPage">
-      <h1>About us</h1>
-      <p className="lead">This project was created by Team named "Wood Divison".<div className=""></div></p>
-      <p>If you have, for example, a flashcard saying: “How many seconds are there in a year?” You would see “Twelve. January 2nd, February 2nd, March 2nd, etc.” on the back side.</p>
-      <p className="lead">
-      <Link to="/categories" className="btn btn-primary">Learn with us</Link>
-      </p>
-    </div>
-    <img src={Wood} alt="..."/>
-  </div>
-);
+const About = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    };
+    return (
+        <div >
+
+            <ReactCardFlip isFlipped={isFlipped}>
+                <Card className="AboutPage">
+                    <CardBody>
+                        <CardTitle tag="h5"><h1>About us</h1></CardTitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">This project was created by Team named "Wood Divison".</CardSubtitle>
+                        <CardText>flashcard example: “How many seconds are there in a year?” You would see “Twelve. January 2nd, February 2nd, March 2nd, etc.” on the back side.</CardText>
+                        <Button className="btn btn-secondary" onClick={handleClick} >Press to learn more</Button>
+                    </CardBody>
+
+                </Card>
+
+                <Card className="AboutPage">
+
+                    <CardBody >
+                        <CardTitle tag="h5"><h1>Wood Division's members:</h1></CardTitle>
+
+                        <CardText> <p className="lead">
+                            <ul>
+                                <li>Adam Project Supervisor</li>
+                                <li>Eddie Backend Manager</li>
+                                <li>FitAdam Frontend Manager</li>
+                                <li>Arnas Backend Developer</li>
+                                <li>Stefano Helper</li>
+                            </ul>
+                        </p></CardText>
+                        <Button className="btn btn-secondary" onClick={handleClick} >Turn back</Button>
+
+                    </CardBody>
+
+                </Card>
+
+            </ReactCardFlip>
+            <img src={Wood} alt="..." />
+
+
+        </div>
+    );
+}
 
 export default connect()(About);
